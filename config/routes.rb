c9093ebc4f5ef8ba0427fd7e55256630c4226784
permanar <richie.permana@gmail.com>
2018-07-18 00:59:02 +0800

    Adding route for accessing API.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   	root 'welcome#test'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    namespace :api do
+      namespace :v1 do
+        resources :lists
+      end
+    end
+    # get '*other', to: redirect('/')
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
