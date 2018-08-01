cebb357cda568cd73a667838b1c1c84b878c6fa3
permanar <richie.permana@gmail.com>
2018-08-01 10:37:49 +0800

    Adding config for CORS.

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,16 @@ module Api
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = false
+
+    config.action_dispatch.default_headers.merge!(
+      'Referrer-Policy' => 'no-referrer'
+    )
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3001'
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
+      end
+    end
   end
 end
